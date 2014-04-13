163514affd2a3d9ef7a5ff242871f66564d6831c
jiristeidl <jiristeidl+github@gmail.com>
2014-04-13 23:30:54 +0200

    BirthDay Class

---
+++ Chapter6/BirthdayPlanner/BirthdayParty.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayPlanner
+{
+    class BirthdayParty
+    {
+        public int NumberOfPeople { get; set; }
+        public bool FancyDecoration { get; set; }
+        const int CostOfFood = 25;        
+        public string CakeWriting { get; set; }
+        private int actualLength
+        {
+            get
+            {
+                if (CakeWriting.Length > MaxWritingLength())
+                    return MaxWritingLength();
+                else
+                    return CakeWriting.Length;
+            }            
+        }
+        public bool CakeWritingTooLong{
+            get{
+                if(CakeWriting.Length > MaxWritingLength())
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+        public decimal Cost
+        {
+            get
+            {
+                decimal totalCost=0;
+                totalCost += NumberOfPeople * CostOfFood;
+                totalCost += CalculateCostOfDecorations();
+                totalCost += (CakeSize() == 8) ? 40M + actualLength * 0.25M : 75M + actualLength * 0.25M;
+                return totalCost;
+
+            }
+        }
+
+        private int CakeSize()
+        {
+            if (NumberOfPeople <= 4)
+                return 8;
+            else
+                return 16;
+        }
+        private int MaxWritingLength()
+        {
+            if (CakeSize() == 8)
+                return 16;
+            else
+                return 40;
+        }
+        private decimal CalculateCostOfDecorations(){
+            decimal total = 0;
+            if(FancyDecoration){
+                total += NumberOfPeople * 15.0M;
+                total += 50.0M;
+            }else{
+                total+=NumberOfPeople*7.5M;
+                total+=30.0M;
+            }
+            return total;
+        }
+        public BirthdayParty(int numberOfPeople, bool fancyDecorations, string cakeWriting)
+        {
+            NumberOfPeople = numberOfPeople;
+            FancyDecoration = fancyDecorations;
+            CakeWriting = cakeWriting;
+        }        
+    }
+}
---
+++ Chapter6/BirthdayPlanner/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace BirthdayPlanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.CostLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.healthyBox = new System.Windows.Forms.CheckBox();
+            this.fancyBox = new System.Windows.Forms.CheckBox();
+            this.numberOfPeople = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfPeople)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(-1, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(286, 264);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.CostLabel);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.healthyBox);
+            this.tabPage1.Controls.Add(this.fancyBox);
+            this.tabPage1.Controls.Add(this.numberOfPeople);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(278, 238);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Dinner Party";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(204, 238);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Birthday Party";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // CostLabel
+            // 
+            this.CostLabel.AutoEllipsis = true;
+            this.CostLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.CostLabel.Location = new System.Drawing.Point(51, 105);
+            this.CostLabel.Name = "CostLabel";
+            this.CostLabel.Size = new System.Drawing.Size(100, 23);
+            this.CostLabel.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Cost";
+            // 
+            // healthyBox
+            // 
+            this.healthyBox.AutoSize = true;
+            this.healthyBox.Location = new System.Drawing.Point(9, 81);
+            this.healthyBox.Name = "healthyBox";
+            this.healthyBox.Size = new System.Drawing.Size(96, 17);
+            this.healthyBox.TabIndex = 9;
+            this.healthyBox.Text = "Healthy Option";
+            this.healthyBox.UseVisualStyleBackColor = true;
+            // 
+            // fancyBox
+            // 
+            this.fancyBox.AutoSize = true;
+            this.fancyBox.Checked = true;
+            this.fancyBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.fancyBox.Location = new System.Drawing.Point(9, 57);
+            this.fancyBox.Name = "fancyBox";
+            this.fancyBox.Size = new System.Drawing.Size(115, 17);
+            this.fancyBox.TabIndex = 8;
+            this.fancyBox.Text = "Fancy Decorations";
+            this.fancyBox.UseVisualStyleBackColor = true;
+            // 
+            // numberOfPeople
+            // 
+            this.numberOfPeople.Location = new System.Drawing.Point(9, 30);
+            this.numberOfPeople.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numberOfPeople.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numberOfPeople.Name = "numberOfPeople";
+            this.numberOfPeople.Size = new System.Drawing.Size(120, 20);
+            this.numberOfPeople.TabIndex = 7;
+            this.numberOfPeople.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Number of People";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(179, 181);
+            this.Controls.Add(this.tabControl1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Party Planner";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfPeople)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label CostLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox healthyBox;
+        private System.Windows.Forms.CheckBox fancyBox;
+        private System.Windows.Forms.NumericUpDown numberOfPeople;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
