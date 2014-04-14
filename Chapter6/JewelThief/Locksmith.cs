883a32219d296949f21d73e8208f31a1c41c6689
jiristeidl <jiristeidl+github@gmail.com>
2014-04-14 19:04:49 +0200

    jewel thief and contructor test

---
+++ Chapter6/JewelThief/Locksmith.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelThief
+{
+    class Locksmith
+    {
+        private string writtenDownCombination;
+        public void OpenSafe(Safe safe, Owner owner)
+        {
+            safe.PickLock(this);
+            Jewels safeContents = safe.Open(writtenDownCombination);
+            ReturnContents(safeContents, owner);
+        }
+
+        virtual public void ReturnContents(Jewels safeContents, Owner owner)
+        {
+            owner.ReceiveContents(safeContents);
+        }
+        internal void WriteDownCombination(string safeCombination)
+        {
+            writtenDownCombination = safeCombination;
+        }
+    }
+}
---
+++ Chapter6/contructorTest/MySubclass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace contructorTest
+{
+    class MySubclass : MyBaseClass
+    {
+        public MySubclass(string baseClassNeedsThis, int anotheValue) : base(baseClassNeedsThis)
+        {
+            MessageBox.Show("This is the subclass: " + baseClassNeedsThis + " and " + anotheValue);
+        }
+    }
+}
